1d157d9eea13d04f0861fa40d13896b05139a78e
gueckmooh <gueckmooh@pm.me>
2019-01-07 15:54:02 +0100

    Get stuff OK

--- src/sudoku.py
+++ src/sudoku.py
@@ -24,6 +24,32 @@ def read_grid (filename):
       grid = grid + [T]
   return grid
 
-filename = "test.txt"
+def check_grid (grid):
+  assert len (grid) == N**2
+  for line in grid:
+    assert len (line) == N**2
+    for value in line:
+      assert value >= 0 and value <= 9
 
-print (read_grid (filename))
+def get_lines (grid):
+  return grid
+
+def get_columns (grid):
+  columns = [[] for i in range (N**2)]
+  for line in grid:
+    for i in range (len (line)):
+      columns[i] = columns[i] + [line [i]]
+  return columns
+
+def get_squares (grid):
+  squares = [[] for i in range (N**2)]
+  for j in range (len (grid)):
+    line = grid [j]
+    for i in range (len (line)):
+      squares[N * (j/3) + (i/3)] += [line[i]]
+  return squares
+
+filename = "example.txt"
+
+grid = read_grid (filename)
+check_grid (grid)
